d20a39eb0bc254bec68de581fc9e0bf65c8a2a37
tlaloc911 <29415755+tlaloc911@users.noreply.github.com>
2024-03-14 10:40:44 -0600

    Update html.js
    Add filter to mask authorization and beautify xml data

--- packages/bruno-cli/src/reporters/html.js
+++ packages/bruno-cli/src/reporters/html.js
@@ -1,7 +1,31 @@
 const fs = require('fs');
 const path = require('path');
+const xmlFormatter = require('xml-formatter');
 
 const makeHtmlOutput = async (results, outputPath) => {
+
+    results.results.forEach(result => {
+		console.log(result)
+	  if (result.request.headers.hasOwnProperty('Authorization')) {
+
+		result.request.headers.Authorization = '*****';
+	  }
+	  
+	  
+	  if(result.response.data[0]=='<')
+	  {
+		  try
+		  {
+			result.response.beautydata = xmlFormatter(result.response.data,{collapseContent: true});
+		  }
+		  catch (error)
+		  {
+			  console.log(error)
+			  result.response.beautydata = ""
+		  }
+	  }
+	});
+  
   const resultsJson = JSON.stringify(results, null, 2);
 
   const reportPath = path.join(__dirname, 'html-template.html');
